0a0ca7cc1753fc60d04bf14040903b873d7f44a7
mayang <644485104@qq.com>
2016-08-03 15:58:52 +0800

    first version

---
+++ helloworld/src/main/java/com/xjj/helloworld/App.java
@@ -0,0 +1,13 @@
+package com.xjj.helloworld;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
